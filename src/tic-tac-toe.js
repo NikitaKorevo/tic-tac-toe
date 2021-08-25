809dab0636ecf8fd517163723d3d7a79de216970
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-08-25 16:11:14 +0300

    feat: done task 7/7

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -7,7 +7,6 @@ class TicTacToe {
                 [null, null, null]
             ],
             whoWinner : null,
-            isDraw: false,
             whoGo: 'x'
         }
     }
@@ -17,9 +16,8 @@ class TicTacToe {
     }
 
     nextTurn(rowIndex, columnIndex) {
-        // меняет state в конструкторе (изменить игрока, обновить хранилище меток и тд)
         let whoGo;
-        
+
         if (this.state.arrTurn[rowIndex][columnIndex] !== null) return;
         if (this.state.whoGo === 'x') {
             whoGo = 'x';
@@ -30,64 +28,53 @@ class TicTacToe {
         }
 
         this.state.arrTurn[rowIndex][columnIndex] = whoGo;
-        this.isFinished();
     }
 
     isFinished() {
-        // должен возвращать истину(TRUE), если игра завершена (например, есть победитель или это ничья) 
+        return this.getWinner() !== null || this.isDraw();
+    }
+
+    getWinner() {
         let copyArrTurn = this.state.arrTurn;
 
         // есть ли победитель по горизонтали
         for (let row = 0; row < copyArrTurn.length; row++) {
             if (copyArrTurn[row][0] === copyArrTurn[row][1] && copyArrTurn[row][0] === copyArrTurn[row][2]) {
-                this.state.whoWinner = copyArrTurn[row][0];
-                return true;
+                return copyArrTurn[row][0];
             }
         }
 
         // есть ли победитель по вертикали
          for (let column = 0; column < copyArrTurn.length; column++) {
             if (copyArrTurn[0][column] === copyArrTurn[1][column] && copyArrTurn[0][column] === copyArrTurn[2][column]) {
-                this.state.whoWinner = copyArrTurn[0][column];
-                return true;
+                return copyArrTurn[0][column];
             }
         }
 
         // есть ли победитель по диагонали
         if (copyArrTurn[0][0] === copyArrTurn[1][1] && copyArrTurn[0][0] === copyArrTurn[2][2]) {
-            this.state.whoWinner = copyArrTurn[0][0];
-            return true;
-        } 
-        if (copyArrTurn[0][2] === copyArrTurn[1][1] && copyArrTurn[0][2] === copyArrTurn[2][0]) {
-            this.state.whoWinner = copyArrTurn[0][2];
-            return true;
+            return copyArrTurn[0][0];
         }
-
-        this.noMoreTurns();
-        return false;
-    }
-
-    getWinner() {
-        // должен возвращать символ победителя (x или o) или null, если победителя еще нет
-        return this.state.whoWinner;
+        if (copyArrTurn[0][2] === copyArrTurn[1][1] && copyArrTurn[0][2] === copyArrTurn[2][0]) {
+            return copyArrTurn[0][2];
+        }
+        return null;
     }
 
     noMoreTurns() {
-        // должен возвращать истину(TRUE), если больше нет полей для размещения x или o
         let copyArrTurn = this.state.arrTurn.flat(1);
 
         for (let i = 0; i < copyArrTurn.length; i++) {
             if (copyArrTurn[i] === null) return false;
         }
-        return this.state.isDraw = true;
+        return true;
     }
 
     isDraw() {
-        return this.state.isDraw;
+        return this.getWinner() === null && this.noMoreTurns();
     }
 
     getFieldValue(rowIndex, colIndex) {
-        // должен возвращать значение матрицы [строка] [столбец] (если есть) или ноль
         return this.state.arrTurn[rowIndex][colIndex];
     }
 }
