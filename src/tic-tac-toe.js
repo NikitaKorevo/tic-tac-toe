97354c5ef41e221b7bf7bb6aa4b7b80c929cf598
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-08-26 10:32:56 +0300

    fix: the game is now over

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -39,23 +39,23 @@ class TicTacToe {
 
         // есть ли победитель по горизонтали
         for (let row = 0; row < copyArrTurn.length; row++) {
-            if (copyArrTurn[row][0] === copyArrTurn[row][1] && copyArrTurn[row][0] === copyArrTurn[row][2]) {
+            if (copyArrTurn[row][0] !== null && copyArrTurn[row][0] === copyArrTurn[row][1] && copyArrTurn[row][0] === copyArrTurn[row][2]) {
                 return copyArrTurn[row][0];
             }
         }
 
         // есть ли победитель по вертикали
          for (let column = 0; column < copyArrTurn.length; column++) {
-            if (copyArrTurn[0][column] === copyArrTurn[1][column] && copyArrTurn[0][column] === copyArrTurn[2][column]) {
+            if (copyArrTurn[0][column] !== null && copyArrTurn[0][column] === copyArrTurn[1][column] && copyArrTurn[0][column] === copyArrTurn[2][column]) {
                 return copyArrTurn[0][column];
             }
         }
 
         // есть ли победитель по диагонали
-        if (copyArrTurn[0][0] === copyArrTurn[1][1] && copyArrTurn[0][0] === copyArrTurn[2][2]) {
+        if (copyArrTurn[0][0] !== null && copyArrTurn[0][0] === copyArrTurn[1][1] && copyArrTurn[0][0] === copyArrTurn[2][2]) {
             return copyArrTurn[0][0];
         }
-        if (copyArrTurn[0][2] === copyArrTurn[1][1] && copyArrTurn[0][2] === copyArrTurn[2][0]) {
+        if (copyArrTurn[0][2] !== null && copyArrTurn[0][2] === copyArrTurn[1][1] && copyArrTurn[0][2] === copyArrTurn[2][0]) {
             return copyArrTurn[0][2];
         }
         return null;
